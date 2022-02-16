583ceae37155f272f39047a415560243c19250a6
amani-joseph <mr.amanijoseph@gmail.com>
2022-02-16 10:23:50 +0300

    Amended exposed api key

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { BehaviorSubject } from 'rxjs';
 
 import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Authorization":environment.githubApiToken
+  })
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -11,10 +17,10 @@ export class ProfileService {
   constructor(private http: HttpClient) {}
   getData(userName: string): Observable<any> {
     const url = `https://api.github.com/users/${userName}`;
-    return this.http.get<any>(url);
+    return this.http.get<any>(url, httpOptions);
   }
   getRepo(userName: string): Observable<any> {
     const url = `https://api.github.com/users/${userName}/repos`;
-    return this.http.get<any>(url);
+    return this.http.get<any>(url, httpOptions);
   }
 }
